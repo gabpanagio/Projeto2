e22d33966215a060e0ccb044b5e78bc88a80c7d5
gabpanagio <panagio33@gmail.com>
2025-07-01 13:25:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirEx1() {
+  window.location.href = 'ex1/indexex1.html';
+}
+
+function abrirEx2() {
+  window.location.href = 'ex2/indexex2.html';
+}
+
+function abrirEx3() {
+  window.location.href = 'ex3/indexex3.html';
+}
