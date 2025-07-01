c38049707b9bdd0bedad1368ec2e8159728c11c2
gabpanagio <panagio33@gmail.com>
2025-07-01 13:43:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function abrirEx1() {
-  window.location.href = 'ex1/indexex1.html';
+  window.location.href = 'indexex1.html';
 }
 
 function abrirEx2() {
-  window.location.href = 'ex2/indexex2.html';
+  window.location.href = 'indexex2.html';
 }
 
 function abrirEx3() {
-  window.location.href = 'ex3/indexex3.html';
+  window.location.href = 'indexex3.html';
 }
